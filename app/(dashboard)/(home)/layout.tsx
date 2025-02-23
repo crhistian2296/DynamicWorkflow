86da682fdb4bb021d0bfb0ae47642c905f2d3d7d
crhistian2296 <garcia.crhistian@correounivalle.edu.co>
2025-02-23 01:33:12 +0100

    feature: theme toggle

--- app/(dashboard)/(home)/layout.tsx
+++ app/(dashboard)/(home)/layout.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import BreadcrumbHeader from "@/components/BreadcrumbHeader";
 import DesktopSidebar from "@/components/Sidebar";
+import { ModeToggle } from "@/components/ThemeModeToggle";
 import { Separator } from "@/components/ui/separator";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
@@ -9,7 +11,10 @@ const layout = ({ children }: { children: React.ReactNode }) => {
       <DesktopSidebar />
       <div className="flex flex-col flex-1 min-h-screen">
         <header className="flex items-center justify-between px-6 py-4 h-[50px] containter">
-          DynamicFlow
+          <BreadcrumbHeader />
+          <div className="gap-1 flex items-center">
+            <ModeToggle />
+          </div>
         </header>
 
         <Separator />
---
+++ components/providers/AppProviders.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import React from "react";
+
+const AppProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default AppProviders;
