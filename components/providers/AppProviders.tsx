06da55ec4392d3d7d0ffe40638f61c2057277b4e
crhistian2296 <garcia.crhistian@correounivalle.edu.co>
2025-03-08 22:45:19 +0100

    feature: queryClient setup

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -1,13 +1,20 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "next-themes";
-import React from "react";
+import React, { useState } from "react";
 
 const AppProviders = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
+        {children}
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
