0c072197e49f34d39fda411c786311b9ccc99e34
crhistian2296 <garcia.crhistian@correounivalle.edu.co>
2025-04-23 02:17:07 +0200

    feat(Editor): add Topbar component for navigation
    Introduce a new Topbar component to the Editor layout, providing a back navigation button for improved user experience. The Topbar is positioned at the top of the editor and includes a tooltip for better usability.

--- app/workflow/_components/Editor.tsx
+++ app/workflow/_components/Editor.tsx
@@ -3,10 +3,13 @@
 import { Workflow } from "@prisma/client";
 import { ReactFlowProvider } from "@xyflow/react";
 import FlowEditor from "./FlowEditor";
+import Topbar from "./topbbar/Topbar";
+
 const Editor = ({ workflow }: { workflow: Workflow }) => {
   return (
     <ReactFlowProvider>
       <div className="flex flex-col h-full w-full overflow-hidden">
+        <Topbar />
         <section className="flex h-full overflow-auto">
           <FlowEditor workflow={workflow} />
         </section>
---
+++ app/workflow/_components/topbbar/Topbar.tsx
@@ -0,0 +1,22 @@
+import TooltipWrapper from "@/components/TooltipWrapper";
+import { Button } from "@/components/ui/button";
+import { ChevronLeftIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const Topbar = () => {
+  const router = useRouter();
+
+  return (
+    <header className="flex p-2 border-p-2 border-separate justify-between w-full h-[60px] sticky top-0 bg-background z-10">
+      <div className="flex flex-1 gap-1">
+        <TooltipWrapper text="Back">
+          <Button variant={"ghost"} size={"icon"} onClick={() => router.back()}>
+            <ChevronLeftIcon size={20} />
+          </Button>
+        </TooltipWrapper>
+      </div>
+    </header>
+  );
+};
+
+export default Topbar;
