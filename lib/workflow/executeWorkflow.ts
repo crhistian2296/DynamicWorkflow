6133d294763d065ef45587e34bc45d54c212313e
crhistian2296 <garcia.crhistian@correounivalle.edu.co>
2025-08-30 17:21:39 +0200

    wip: workFlow execution in the sever side 2

--- lib/workflow/executeWorkflow.ts
+++ lib/workflow/executeWorkflow.ts
@@ -1,9 +1,28 @@
+import { revalidatePath } from "next/cache";
 import "server-only";
 import prisma from "../prisma";
 
 export async function ExecuteWorkflow(executionId: string) {
-  const execution = prisma.workflowExecution.findUnique({
+  const execution = await prisma.workflowExecution.findUnique({
     where: { id: executionId },
     include: { workflow: true, phases: true },
   });
+
+  if (!execution) throw new Error("Execution not found");
+
+  // TODO: setup execution environment
+
+  // TODO: initialize workflow execution
+
+  // TODO: initialize phases status
+
+  let executionFailed = false;
+  for (const phase of execution.phases) {
+    // TODO: execute each phase
+  }
+
+  // TODO: finalize execution
+  // TODO:cleanup execution environment
+
+  revalidatePath("workflow/runs");
 }
