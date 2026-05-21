61ba7816446d71c738bee29d4f541e8497db12fe
crhistian2296 <garcia.crhistian@correonivalle.edu.co>
2026-05-21 22:58:36 +0200

    testing new db

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,7 @@
-import { PrismaLibSql } from "@prisma/adapter-libsql";
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  const adapter = new PrismaLibSql({ url: process.env.DATABASE_URL! });
-  return new PrismaClient({ adapter });
+  return new PrismaClient();
 };
 
 declare const globalThis: {
