367b6d9b347594bf2c7bd7d81dd23186b0632fba
crhistian2296 <garcia.crhistian@correonivalle.edu.co>
2026-05-23 20:47:52 +0200

    fix: vercel resolve path

--- lib/helper/appUrl.ts
+++ lib/helper/appUrl.ts
@@ -1,4 +1,10 @@
 export function getAppUrl(path: string) {
-  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+  const localUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+  const vercelUrl = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : undefined;
+
+  const baseUrl =
+    process.env.NODE_ENV === "development" ? localUrl : vercelUrl || localUrl;
   return `${baseUrl}/${path}`;
 }
